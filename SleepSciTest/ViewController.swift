827b0d2757f96768045cf7f569285547e3b20a0d
AlexGood <sasha2286980@gmail.com>
2018-11-13 21:37:24 +0200

    Add square transform
    + UI squareView
    + squareView transform
    + simulate data for transform

--- SleepSciTest/ViewController.swift
+++ SleepSciTest/ViewController.swift
@@ -8,13 +8,59 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-
+//Test enum
+private enum BreathType: CGFloat {
+    case inhale = 1
+    case prepare = 0.75
+    case exhale = 0.5
 }
 
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var breetheContainer: UIView!
+    @IBOutlet weak var animatingView: UIView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var timerLabel: UILabel!
+    
+    private let animationDuration: TimeInterval = 2
+    private var breathType: BreathType = .prepare
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupAnimatingView()
+    }
+    
+    //MARK:- Private
+    private func setupAnimatingView() {
+        animateBreathSquare(with: .inhale)
+    }
+    
+    private func animateBreathSquare(with scalePoint: BreathType) {
+        let scale = scalePoint.rawValue
+        animatingView.transform = CGAffineTransform(scaleX: scale, y: scale)
+    }
+    
+    private func startAnimation() {
+        UIView.animate(withDuration: animationDuration, animations: {
+            switch self.breathType {
+            case .prepare:
+                self.animateBreathSquare(with: .prepare)
+                self.breathType = .inhale
+            case .inhale:
+                self.animateBreathSquare(with: .inhale)
+                self.breathType = .exhale
+            case .exhale:
+                self.animateBreathSquare(with: .exhale)
+                self.breathType = .inhale
+            }
+        }, completion: { _ in
+            self.startAnimation()
+        })
+    }
+    
+    //MARK:- IBActions
+    @IBAction func startBreathing(_ sender: Any) {
+        startAnimation()
+    }
+}
